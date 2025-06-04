77a12a3b8f8c6d6325750e821f1d6c3788d92ea4
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-04 18:51:25 -0300

    feat: add service to upgrade order stage

---
+++ internal/core/services/upgradeOrderStage.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"github.com/rogerioisj/soat-project/internal/core/domain"
+	"github.com/rogerioisj/soat-project/internal/core/ports/repositories"
+)
+
+type UpgradeOrderStageService struct {
+	or repositories.OrderRepositoryInterface
+}
+
+func NewUpgradeOrderStageService(or repositories.OrderRepositoryInterface) *UpgradeOrderStageService {
+	return &UpgradeOrderStageService{
+		or: or,
+	}
+}
+
+func (s *UpgradeOrderStageService) Execute(orderId string) *domain.DomainError {
+	o, err := s.or.GetById(orderId)
+	if err != nil {
+		return err
+	}
+
+	stage := o.GetStatus()
+
+	stage = defineNewStage(stage)
+
+	err = o.SetStatus(stage)
+
+	if err != nil {
+		return err
+	}
+
+	err = s.or.Update(o)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func defineNewStage(stage domain.OrderStatus) domain.OrderStatus {
+	switch stage {
+	case domain.Building:
+		return domain.Received
+	case domain.Received:
+		return domain.Preparing
+	case domain.Preparing:
+		return domain.Ready
+	case domain.Ready:
+		return domain.Done
+	default:
+		return stage // No change for Done or Cancelled
+	}
+}
