3853492d81e22521fb748e5f95d9e9ca72fc2443
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-04 18:31:42 -0300

    refactor: change method

--- internal/core/ports/repositories/order.go
+++ internal/core/ports/repositories/order.go
@@ -5,5 +5,5 @@ import "github.com/rogerioisj/soat-project/internal/core/domain"
 type OrderRepositoryInterface interface {
 	Create(order *domain.Order) *domain.DomainError
 	GetById(id string) (*domain.Order, *domain.DomainError)
-	UpdateStatus(id string, status domain.OrderStatus) *domain.DomainError
+	Update(order *domain.Order) *domain.DomainError
 }
