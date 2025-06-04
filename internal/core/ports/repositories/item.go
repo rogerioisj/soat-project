47660e1d666ac60f57778942c7e4e307057396fe
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-04 18:31:25 -0300

    refactor: change return type

--- internal/core/ports/repositories/item.go
+++ internal/core/ports/repositories/item.go
@@ -3,7 +3,7 @@ package repositories
 import "github.com/rogerioisj/soat-project/internal/core/domain"
 
 type ItemRepositoryInterface interface {
-	Create(item *domain.Item) error
-	GetById(id string) (*domain.Item, error)
-	ListByType(productType string) ([]*domain.Item, error)
+	Create(item *domain.Item) *domain.DomainError
+	GetById(id string) (*domain.Item, *domain.DomainError)
+	ListByType(productType string) ([]*domain.Item, *domain.DomainError)
 }
