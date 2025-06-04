369e72863041e67d5d4219eac6c813fba81405ba
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-04 10:48:10 -0300

    refactor: change port

--- internal/core/ports/repositories/user.go
+++ internal/core/ports/repositories/user.go
@@ -3,7 +3,8 @@ package repositories
 import "github.com/rogerioisj/soat-project/internal/core/domain"
 
 type UserRepositoryInterface interface {
-	Create(user *domain.User) error
-	GetByCpf(cpf string) (*domain.User, error)
-	GetByEmail(email string) (*domain.User, error)
+	Create(user *domain.User) *domain.DomainError
+	GetByCpf(user *domain.User, cpf string) *domain.DomainError
+	GetByEmail(user *domain.User) *domain.DomainError
+	GetByCpfOrEmail(user *domain.User) *domain.DomainError
 }
