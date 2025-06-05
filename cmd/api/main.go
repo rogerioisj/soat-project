824ac12bb1848f99df911b859738eb2b3d1c204d
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-04 23:44:06 -0300

    feat: inject dependencies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/rogerioisj/soat-project/internal/adapters/inbound/http/handlers"
+	"github.com/rogerioisj/soat-project/internal/adapters/outbound/repositories/postgres"
+	"github.com/rogerioisj/soat-project/internal/core/services/user"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	})
+	db := dataBaseConnection()
+	defer db.Close()
+
+	userRepository := postgres.NewUserRepository(db)
+
+	identifyWithCpfService := user.NewIdentifyWithCpfService(userRepository)
+
+	uh := handlers.NewGetUserByCpfHandler(identifyWithCpfService)
+
+	http.HandleFunc("GET /user", uh.Execute)
 
 	fmt.Println("Listening on port 8080")
 	fmt.Println("GET http://127.0.0.1:8080")
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func dataBaseConnection() *sql.DB {
+	connStr := "user=admin password=admin dbname=restaurant_db sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
 }
