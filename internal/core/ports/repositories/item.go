3c4bd668e3434464263bcab87b31bc9e24b20e9a
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-05 14:01:51 -0300

    refactor: change params

--- internal/core/ports/repositories/item.go
+++ internal/core/ports/repositories/item.go
@@ -4,6 +4,6 @@ import "github.com/rogerioisj/soat-project/internal/core/domain"
 
 type ItemRepositoryInterface interface {
 	Create(item *domain.Item) *domain.DomainError
-	GetById(id string) (*domain.Item, *domain.DomainError)
-	ListByType(productType string, page, limit int32) ([]*domain.Item, *domain.DomainError)
+	GetById(id string, item *domain.Item) *domain.DomainError
+	ListByType(productType string, page, limit int32, itemList *[]domain.Item) *domain.DomainError
 }
