3ec7d952db0fe9262663d8f12132273cbda32896
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-05 18:46:57 -0300

    refactor: change param

--- internal/core/ports/repositories/order.go
+++ internal/core/ports/repositories/order.go
@@ -4,6 +4,6 @@ import "github.com/rogerioisj/soat-project/internal/core/domain"
 
 type OrderRepositoryInterface interface {
 	Create(order *domain.Order) *domain.DomainError
-	GetById(id string) (*domain.Order, *domain.DomainError)
+	GetById(id string, order *domain.Order) *domain.DomainError
 	Update(order *domain.Order) *domain.DomainError
 }
