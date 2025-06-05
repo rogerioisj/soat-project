f47857356bddd3f5270e1ce85b079806ef8c6fb9
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-05 18:49:54 -0300

    feat: add route to create orders

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/rogerioisj/soat-project/internal/adapters/inbound/http/handlers"
 	"github.com/rogerioisj/soat-project/internal/adapters/outbound/repositories/postgres"
 	"github.com/rogerioisj/soat-project/internal/core/services/item"
+	"github.com/rogerioisj/soat-project/internal/core/services/order"
 	"github.com/rogerioisj/soat-project/internal/core/services/user"
 	"log"
 	"net/http"
@@ -18,19 +19,26 @@ func main() {
 
 	userRepository := postgres.NewUserRepository(db)
 	itemRepository := postgres.NewItemRepository(db)
+	orderRepository := postgres.NewOrderRepository(db)
 
 	identifyWithCpfService := user.NewIdentifyWithCpfService(userRepository)
 	createUserService := user.NewCreateUserService(userRepository)
+
 	getItensService := item.NewGetItensBasedOnCategoryService(itemRepository)
+
+	createOrderService := order.NewCreateOrderService(orderRepository, userRepository)
 
 	guh := handlers.NewGetUserByCpfHandler(identifyWithCpfService)
 	cuh := handlers.NewCreateUser(createUserService)
 	gih := handlers.NewGetItensHandler(getItensService)
+	coh := handlers.NewCreateOrderHandler(createOrderService)
 
 	http.HandleFunc("GET /user", guh.Execute)
 	http.HandleFunc("POST /user", cuh.Execute)
 
 	http.HandleFunc("GET /itens", gih.Execute)
+
+	http.HandleFunc("POST /order", coh.Execute)
 
 	fmt.Println("Listening on port 8080")
 	fmt.Println("GET http://127.0.0.1:8080")
