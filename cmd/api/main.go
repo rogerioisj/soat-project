90d9029a3de87df42ff2b5df3da1ffdb0dae2cd0
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-05 14:03:43 -0300

    feat: add route to get itens

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/rogerioisj/soat-project/internal/adapters/inbound/http/handlers"
 	"github.com/rogerioisj/soat-project/internal/adapters/outbound/repositories/postgres"
+	"github.com/rogerioisj/soat-project/internal/core/services/item"
 	"github.com/rogerioisj/soat-project/internal/core/services/user"
 	"log"
 	"net/http"
@@ -16,15 +17,20 @@ func main() {
 	defer db.Close()
 
 	userRepository := postgres.NewUserRepository(db)
+	itemRepository := postgres.NewItemRepository(db)
 
 	identifyWithCpfService := user.NewIdentifyWithCpfService(userRepository)
 	createUserService := user.NewCreateUserService(userRepository)
+	getItensService := item.NewGetItensBasedOnCategoryService(itemRepository)
 
 	guh := handlers.NewGetUserByCpfHandler(identifyWithCpfService)
 	cuh := handlers.NewCreateUser(createUserService)
+	gih := handlers.NewGetItensHandler(getItensService)
 
 	http.HandleFunc("GET /user", guh.Execute)
 	http.HandleFunc("POST /user", cuh.Execute)
+
+	http.HandleFunc("GET /itens", gih.Execute)
 
 	fmt.Println("Listening on port 8080")
 	fmt.Println("GET http://127.0.0.1:8080")
