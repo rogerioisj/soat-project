db871f8a89720a6efe576e2dfbbe65bfdd3be433
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-05 00:29:30 -0300

    refactor: change param

--- internal/core/ports/repositories/user.go
+++ internal/core/ports/repositories/user.go
@@ -5,7 +5,7 @@ import "github.com/rogerioisj/soat-project/internal/core/domain"
 type UserRepositoryInterface interface {
 	Create(user *domain.User) *domain.DomainError
 	GetByCpf(user *domain.User, cpf string) *domain.DomainError
-	GetByEmail(user *domain.User) *domain.DomainError
+	GetByEmail(user *domain.User, email string) *domain.DomainError
 	GetByCpfOrEmail(user *domain.User) *domain.DomainError
 	GetGuestUser(user *domain.User) *domain.DomainError
 }
