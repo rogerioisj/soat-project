e3e21d20af6797a387159578a450e61a534fb99e
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-04 22:21:04 -0300

    chore: add new migration for order table

---
+++ migrations/000003_create_order_table.up.sql
@@ -0,0 +1,26 @@
+CREATE TYPE order_status AS ENUM (
+    'pending',
+    'preparing',
+    'ready',
+    'delivered',
+    'cancelled',
+    'done'
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    status order_status NOT NULL DEFAULT 'pending',
+    user_id INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+);
+
+CREATE TABLE orders_itens (
+    order_id INTEGER NOT NULL,
+    item_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL DEFAULT 1,
+    PRIMARY KEY (order_id, item_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES itens(id) ON DELETE CASCADE
+);
