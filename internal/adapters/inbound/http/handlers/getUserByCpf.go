4270b417473daf0855496c81a558c55e92419760
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-04 23:43:44 -0300

    feat: create method to handle with http requests

--- internal/adapters/inbound/http/handlers/getUserByCpf.go
+++ internal/adapters/inbound/http/handlers/getUserByCpf.go
@@ -2,25 +2,53 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/rogerioisj/soat-project/internal/core/domain"
+	"github.com/rogerioisj/soat-project/internal/core/services/user"
 	"log"
 	"net/http"
 )
 
-func GetUserByCpfHandler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Method not allowed:", r.Method)
+type GetUserByCpfHandler struct {
+	s user.IdentifyWithCpfServiceInterface
+}
 
+func NewGetUserByCpfHandler(s user.IdentifyWithCpfServiceInterface) *GetUserByCpfHandler {
+	return &GetUserByCpfHandler{
+		s: s,
+	}
+}
+
+func (h *GetUserByCpfHandler) Execute(w http.ResponseWriter, r *http.Request) {
 	cpf := r.URL.Query().Get("cpf")
-
-	fmt.Println("Received request to get user by CPF:", cpf)
 
 	if cpf == "" {
 		http.Error(w, "CPF is required", http.StatusBadRequest)
 		return
 	}
 
+	if len(cpf) < 11 {
+		http.Error(w, "CPF must be at least 11 characters long", http.StatusBadRequest)
+		return
+	}
+
+	u, err := h.s.Execute(cpf)
+
+	if err != nil && err.Is(domain.UserNotFound) {
+		http.Error(w, "", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		log.Print("Error retrieving user by CPF: ", err)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
 	response := map[string]interface{}{
-		"CPF": cpf,
+		"ID":    u.GetID(),
+		"CPF":   u.GetCPF(),
+		"Name":  u.GetName(),
+		"Email": u.GetEmail(),
 	}
 
 	w.Header().Set("Content-Type", "application/json")
