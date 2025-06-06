eca357d46efffda3ae5c1def715a45aa59f1ae33
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-06 15:28:07 -0300

    refactor: add order by in query

--- internal/adapters/outbound/repositories/postgres/item.go
+++ internal/adapters/outbound/repositories/postgres/item.go
@@ -52,7 +52,7 @@ func (r *ItemRepository) GetById(id string, item *domain.Item) *domain.DomainErr
 }
 
 func (r *ItemRepository) ListByType(productType string, page, limit int32, itemList *[]domain.Item) *domain.DomainError {
-	rows, err := r.db.Query("SELECT id, name, description, price FROM itens WHERE type = $1 LIMIT $2 OFFSET $3", productType, limit, (page-1)*limit)
+	rows, err := r.db.Query("SELECT id, name, description, price FROM itens WHERE type = $1 ORDER BY id LIMIT $2 OFFSET $3", productType, limit, (page-1)*limit)
 
 	if err != nil {
 		log.Printf("Error querying items by type: %v", err)
@@ -76,6 +76,38 @@ func (r *ItemRepository) ListByType(productType string, page, limit int32, itemL
 
 		*itemList = append(*itemList, item)
 	}
+
+	return nil
+}
+
+func (r *ItemRepository) Update(item *domain.Item, id string) *domain.DomainError {
+	transaction, err := r.db.Begin()
+	if err != nil {
+		log.Printf("Error starting transaction: %v", err)
+		return domain.NewDomainError("Database Error", "Error starting transaction")
+	}
+
+	_, err = transaction.Exec("UPDATE itens SET name = $1, description = $2, price = $3, type = $4 WHERE id = $5",
+		item.GetName(),
+		item.GetDescription(),
+		item.GetPrice(),
+		item.GetProductType(),
+		id,
+	)
+	if err != nil {
+		log.Printf("Error updating item: %v", err)
+		if rollbackErr := transaction.Rollback(); rollbackErr != nil {
+			log.Printf("Error rolling back transaction: %v", rollbackErr)
+		}
+		return domain.NewDomainError("Database Error", "Error updating item")
+	}
+
+	if err := transaction.Commit(); err != nil {
+		log.Printf("Error committing transaction: %v", err)
+		return domain.NewDomainError("Database Error", "Error committing transaction")
+	}
+
+	log.Printf("Item %s updated successfully", item.GetName())
 
 	return nil
 }
