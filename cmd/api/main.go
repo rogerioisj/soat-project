738a845c04582c6b1a2816bba3734187ea915ac2
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-06-06 11:13:45 -0300

    feat: add route to get active orders

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/rogerioisj/soat-project/internal/adapters/inbound/http/handlers"
 	"github.com/rogerioisj/soat-project/internal/adapters/outbound/repositories/postgres"
@@ -28,12 +27,14 @@ func main() {
 
 	createOrderService := order.NewCreateOrderService(orderRepository, userRepository)
 	upgradeOrderService := order.NewUpgradeOrderStageService(orderRepository)
+	listActiveOrdersService := order.NewListOrdersService(orderRepository)
 
 	guh := handlers.NewGetUserByCpfHandler(identifyWithCpfService)
 	cuh := handlers.NewCreateUser(createUserService)
 	gih := handlers.NewGetItensHandler(getItensService)
 	coh := handlers.NewCreateOrderHandler(createOrderService)
 	uoh := handlers.NewUpgradeOrderStageHandler(upgradeOrderService)
+	gaoh := handlers.NewGetOrdersHandler(listActiveOrdersService)
 
 	http.HandleFunc("GET /user", guh.Execute)
 	http.HandleFunc("POST /user", cuh.Execute)
@@ -42,18 +43,26 @@ func main() {
 
 	http.HandleFunc("POST /order", coh.Execute)
 	http.HandleFunc("PATCH /order", uoh.Execute)
+	http.HandleFunc("GET /orders", gaoh.Execute)
 
-	fmt.Println("Listening on port 8080")
-	fmt.Println("GET http://127.0.0.1:8080")
+	log.Println("Listening on port 8080")
+	log.Println("GET http://127.0.0.1:8080")
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func dataBaseConnection() *sql.DB {
+	log.Println("Connecting to database ...")
 	connStr := "user=admin password=admin dbname=restaurant_db sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	} else {
+		log.Println("Database connection established successfully.")
 	}
 
 	return db
